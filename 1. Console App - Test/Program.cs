4e88d5d692d2ee370440aec94eb0f198b7c5fbde
Rajiv Gogia <er.rajivgogia7@gmail.com>
2026-05-09 21:13:18 +0530

    second commit9

--- 1. Console App - Test/Program.cs
+++ 1. Console App - Test/Program.cs
@@ -72,9 +72,21 @@ class Calculator
                         result = Divide(num1, num2);
                         Console.WriteLine($"\nResult: {num1} / {num2} = {result}");
                     }
-                    break;               
+                    break; 
+                    case "4":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("\nError: Cannot divide by zero.");
+                        validOperation = false;
+                    }
+                    else
+                    {
+                        result = Divide(num1, num2);
+                        Console.WriteLine($"\nResult: {num1} / {num2} = {result}");
+                    }
+                    break;                
             }
-            
+
         }
     }
 
